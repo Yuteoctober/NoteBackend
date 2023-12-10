3f109f162ad5a1bd997416782ed4250721690e29
Yute Lilitprapun <yudthsoponvit@gmail.com>
2023-12-09 20:31:47 -0500

    .

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userRouter = require('./routes/auth');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const cardRouter = require('./routes/cardroute');
-const checklistRouter = require('./routes/checklistRouter');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const userRouter = require('./routes/auth')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+const cardRouter = require('./routes/cardroute')
+const checklistRouter = require('./routes/checklistRouter')
 
-const app = express();
+const app = express()
 
 app.use(cors({
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}));
-app.use(cookieParser());
-app.use(express.json());
-dotenv.config();
-app.use('/auth', userRouter);
-app.use('/card', cardRouter);
-app.use('/checklist', checklistRouter);
+}))
+app.use(cookieParser())
+app.use(express.json())
+dotenv.config()
+app.use('/auth', userRouter)
+app.use('/card', cardRouter)
+app.use('/checklist', checklistRouter)
 
-mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('MongoDB connected');
-        const port = process.env.PORT || 8080;
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch((error) => {
-        console.error('MongoDB connection error:', error);
-    });
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Starting on port 8080', 'server connected')
+    app.listen(8080)
+})
