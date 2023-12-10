301e1e4c9e3ced18b778eb72c1b758eb444a1dc1
Yute Lilitprapun <yudthsoponvit@gmail.com>
2023-12-09 21:10:59 -0500

    .

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const checklistRouter = require('./routes/checklistRouter')
 
 const app = express()
 
-app.use(cors({
-    origin: ['http://localhost:5173/'],
+const corsOptions ={
+    origin:['http://localhost:5173/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}))
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
 app.use(cookieParser())
 app.use(express.json())
 dotenv.config()
